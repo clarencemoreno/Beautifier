966260547f5ff0b632f48596d7b28bbf572a8a33
Clyde Moreno <clydemoreno@mac.com>
2026-08-01 19:14:40 +0800

    Initial Commit

---
+++ BeautifierTests/BeautifierTests.swift
@@ -0,0 +1,18 @@
+//
+//  BeautifierTests.swift
+//  BeautifierTests
+//
+//  Created by ClyCesBon on 8/1/26.
+//
+
+import Testing
+
+struct BeautifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
